820062cec054be883acf293d4d5194928e0dd4e8
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-07 15:38:05 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+  #Pirates should have a name, weight, and height. You will also need a class method that returns all the pirates. T
+  attr_accessor :name, :weight, :height
+  PIRATES = []
+
+  def initialize(name, weight, height)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  #The ship class should have name, type, and booty attributes,
+  #as well as a class method .all that returns all the ships and a class method .clear that deletes all ships.
+  attr_accessor :name, :type, :booty
+  SHIPS = []
+
+  def initialize(name, type, booty)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    SHIPS << self
+  end
+
+  def self.all
+    SHIPS
+  end
+
+  def self.clear
+    SHIPS.clear
+  end
 end
