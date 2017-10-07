82887756453a960ca76b421ae380db9ac0a69970
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-07 15:19:17 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,12 +1,16 @@
 class Pirate
   #Pirates should have a name, weight, and height. You will also need a class method that returns all the pirates. T
   attr_accessor :name, :weight, :height
-  @@all = []
+  PIRATES = []
 
   def initialize(name, weight, height)
     @name = name
     @weight = weight
     @height = height
-    @@all << self
+    PIRATES << self
+  end
+
+  def self.all 
+    PIRATES
   end
 end
